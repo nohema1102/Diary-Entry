eba8f8231da0c8d2f4a146538e5b8745d32c926a
Nohemí <nohemi1180212345@gmail.com>
2021-06-08 02:29:44 -0600

    Se agrega el program de la P8 pero aún hay errores

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Globalization;
+
+namespace Diary_Entry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("¡Hola! Bienvenido a tu diario, ¿Qué está pasando hoy? [Por favor, empieza a escribir]");
+             string textoDiario = Console.ReadLine();
+
+             DateTime hoy = DateTime.Now;
+             CultureInfo cultura = new CultureInfo("es-MX");
+             string hoyString = hoy.ToString(cultura);
+
+             using (StreamWriter sw = new StreamWriter(hoyString))
+             {
+                 sw.WriteLine(textoDiario);
+             }
+            Console.WriteLine(hoyString);
+        }
+    }
+}
