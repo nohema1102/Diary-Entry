97e19c15e8512ffb04398fac8bdb2e6cc4634778
Nohemí <nohemi1180212345@gmail.com>
2021-06-09 11:53:34 -0600

    Se terminó el programa

--- Program.cs
+++ Program.cs
@@ -8,18 +8,21 @@ namespace Diary_Entry
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("¡Hola! Bienvenido a tu diario, ¿Qué está pasando hoy? [Por favor, empieza a escribir]");
-             string textoDiario = Console.ReadLine();
-
+            
              DateTime hoy = DateTime.Now;
              CultureInfo cultura = new CultureInfo("es-MX");
-             string hoyString = hoy.ToString(cultura);
+             string hoyString = hoy.ToString("d/M/yyyy");
+             string hoyReplace = hoyString.Replace("/", "-"); 
 
-             using (StreamWriter sw = new StreamWriter(hoyString))
+             Console.WriteLine("¡Hola! Bienvenido a tu diario, ¿Qué está pasando hoy? [Por favor, empieza a escribir]");
+             string textoDiario = Console.ReadLine();
+
+
+             using (StreamWriter sw = new StreamWriter(hoyReplace))
              {
                  sw.WriteLine(textoDiario);
              }
-            Console.WriteLine(hoyString);
+          
         }
     }
 }
